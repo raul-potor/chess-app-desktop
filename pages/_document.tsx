3b0d4caa749d5d7b7931534b5bb815b0e0624117
Raul Potor <raul.potor@gmail.com>
2023-02-11 21:40:56 +0200

    feat: fix analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,16 @@ export default function Document() {
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { page_path: window.location.pathname });
+            `,
+          }}
+        />
       </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
